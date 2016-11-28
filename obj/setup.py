348e96a1343526f67b862a13b06db2d50a9a1a7b
Loïc Grobol <loic.grobol@gmail.com>
2016-11-28 16:59:02 +0100

    Fix #4

--- obj/setup.py
+++ obj/setup.py
@@ -22,46 +22,23 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import re, os.path
-join = os.path.join
+import re
+import os
+import os.path
 
-def get_modules(path):
-    """
-    Return the list of modules that can be called from command-line.
-    They are sniffed in subdirectories of "src" containing
-    an __init__.py file.
-    """
-    
-    def to_module_name(name):
-        return re.sub(r"[\\/]", ".", re.sub(r"^[./\\]+", "", elt[:-3], re.MULTILINE))
-    
-    def get(path):
-        ids    = os.listdir(path)
-        pathes = []
-        
-        if "__init__.py" in ids:
-            ids    = sorted([module for module in ids if module.endswith(".py") and not module.startswith("_")])
-            pathes = [join(path, module) for module in ids]
-            ids    = [module[:-3] for module in ids]
-            
-            dirs   = [join(path, d) for d in os.listdir(path) if os.path.isdir(join(path, d))]
-            
-            for i in dirs:
-                L, R = get(i)
-                for i in xrange(len(L)):
-                    m = L[i]
-                    if m not in ids:
-                        ids.append(m)
-                    else:
-                        raise ValueError("Module indexation crash, module \"" + m + "\" found twice:\n" +
-                                         "    " + pathes[ids.index(m)] + "\n" +
-                                         "    " + R[i])
-                pathes.extend(R)
-        else:
-            del ids[:]
-        
-        return [ids, pathes]
-    ids, pathes = get(path)
-    pathes      = [to_module_name(elt) for elt in pathes]
-    
-    return [ids,pathes]
+
+def get_modules(path, root):
+    def to_module_name(path):
+        return os.path.splitext(os.path.relpath(path, root))[0].replace(os.path.sep, '.')
+
+    def get(modules_path):
+        res = dict()
+        for dirpath, _, filenames in os.walk(modules_path):
+            if '__init__.py' in filenames:
+                modules_path = (f for f in filenames
+                                if os.path.splitext(f)[1] == '.py' and not os.path.basename(f).startswith('_'))
+                res.update({os.path.splitext(os.path.basename(m))[0]: os.path.join(dirpath, m) for m in modules_path})
+
+        return res
+    modules = get(os.path.join(root, path))
+    return {m: to_module_name(p) for m, p in modules.items()}
